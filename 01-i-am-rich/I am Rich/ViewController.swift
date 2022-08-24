22a8bd80751d96bf1c1815d9ea1735fda5bdcd5f
Bokkoa <krivozznova@gmail.com>
2022-08-23 21:06:03 -0700

    First commit

---
+++ 01-i-am-rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Felipe on 23/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,4 @@
+# iOS & Swift - The Complete iOS App Development Bootcamp
+
+This is an example made while i took Angela Yu's course.
+you can check it here -> [https://www.udemy.com/course/ios-13-app-development-bootcamp/](https://www.udemy.com/course/ios-13-app-development-bootcamp/)
