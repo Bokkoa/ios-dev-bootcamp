423fa514040852e75b6546814d63b0d3ccb0b96c
Felipe E Learning <bokkoa@Ulisess-MacBook-Pro.local>
2022-09-04 12:59:00 -0500

    Finishing second excersise

---
+++ 02-exercise-Magic-8-ball/Magic 8 Ball/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Angela Yu on 14/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    
+    @IBOutlet weak var ballImage: UIImageView!
+    
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        ballImage.image = ballArray[Int.random(in: 0...4)]
+    }
+    
+
+}
+
